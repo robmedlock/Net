e8648989972c79bea40c2da5f7344a8d7dd2fcb4
Simon Dineen <mailbox@javaconsult.co.uk>
2018-11-23 21:53:50 +0000

    update

--- C# Solution/Examples/Maths.cs
+++ C# Solution/Examples/Maths.cs
@@ -6,25 +6,6 @@ namespace Examples
 {
     public class Maths
     {
-        public static string ToRoman(int number)
-        {
-            if ((number < 1 || (number > 4999))) return string.Empty;
-            if (number >= 1000) return "M" + ToRoman(number - 1000);
-            if (number >= 900) return "CM" + ToRoman(number - 900);
-            if (number >= 500) return "D" + ToRoman(number - 500);
-            if (number >= 400) return "CD" + ToRoman(number - 400);
-            if (number >= 100) return "C" + ToRoman(number - 100);
-            if (number >= 90) return "XC" + ToRoman(number - 90);
-            if (number >= 50) return "L" + ToRoman(number - 50);
-            if (number >= 40) return "XL" + ToRoman(number - 40);
-            if (number >= 10) return "X" + ToRoman(number - 10);
-            if (number >= 9) return "IX" + ToRoman(number - 9);
-            if (number >= 5) return "V" + ToRoman(number - 5);
-            if (number >= 4) return "IV" + ToRoman(number - 4);
-            if (number >= 1) return "I" + ToRoman(number - 1);
-            return string.Empty;
-        }
-
         public static double Combination(int n, int r)
         {
            return Factorial(n) / (Factorial(r) * Factorial(n - r));
@@ -70,6 +51,39 @@ namespace Examples
             return sequence;
         }
 
+        //Recursive methods
 
+        public static double RecursiveFactorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1; //base case
+            }
+            else
+            {
+                return n * RecursiveFactorial(n - 1);
+            }
+            //return n==1 ? 1 : n * Factorial(n - 1);
+        }
+
+
+        public static string ToRoman(int number)
+        {
+            //if ((number < 1 || (number > 4999))) return string.Empty;
+            if (number >= 1000) return "M" + ToRoman(number - 1000);
+            if (number >= 900) return "CM" + ToRoman(number - 900);
+            if (number >= 500) return "D" + ToRoman(number - 500);
+            if (number >= 400) return "CD" + ToRoman(number - 400);
+            if (number >= 100) return "C" + ToRoman(number - 100);
+            if (number >= 90) return "XC" + ToRoman(number - 90);
+            if (number >= 50) return "L" + ToRoman(number - 50);
+            if (number >= 40) return "XL" + ToRoman(number - 40);
+            if (number >= 10) return "X" + ToRoman(number - 10);
+            if (number >= 9) return "IX" + ToRoman(number - 9);
+            if (number >= 5) return "V" + ToRoman(number - 5);
+            if (number >= 4) return "IV" + ToRoman(number - 4);
+            if (number >= 1) return "I" + ToRoman(number - 1);
+            return string.Empty; //base case
+        }
     }
 }
