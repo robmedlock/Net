c5636125d6aa8fc7839c142c967fd0b8526532b5
SimonDineen <mailbox@javaconsult.co.uk>
2019-10-11 11:21:13 +0100

    fri

--- C# Solution1/Solution1/ConsoleApp/Program.cs
+++ C# Solution1/Solution1/ConsoleApp/Program.cs
@@ -9,10 +9,14 @@ namespace ConsoleApp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            new ClassLibrary.Repository.EF.ProductRepository(
-                new ClassLibrary.Repository.EF.EcommerceContext()).SelectAll().ToList().ForEach(p => Console.WriteLine(p.Name));
+
+            Enumerable.Range(1, 10).ToList().ForEach(Console.WriteLine);
+
+            //new ClassLibrary.Repository.EF.ProductRepository(
+            //    new ClassLibrary.Repository.EF.EcommerceContext()).SelectAll().ToList().ForEach(p => Console.WriteLine(p.Name));
 
             //string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=ecommerce;User ID=sa;Password=carpond";
             //new ClassLibrary.Repository.Sql.SqlProductRepository(connectionString).SelectAll().ToList().ForEach(p => Console.WriteLine(p.Name));
